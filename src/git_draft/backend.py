d2c2dc96c620a8824bf77efdeed8d5cc7b308fa2
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-02-26 22:01:26 -0800

    fixup! feat: add initial OpenAI backend

--- src/git_draft/backend.py
+++ src/git_draft/backend.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import dataclasses
 import openai
 from pathlib import PurePosixPath
+import textwrap
 from typing import Protocol, Sequence
 
 
@@ -28,13 +29,27 @@ class Backend:
         raise NotImplementedError()
 
 
+# https://aider.chat/docs/more-info.html
+# https://github.com/Aider-AI/aider/blob/main/aider/prompts.py
+_SYSTEM_PROMPT = textwrap.dedent(
+    """
+    You are an expert software engineer, who writes correct and concise code.
+"""
+)
+
+
 class OpenAIBackend(Backend):
     def __init__(self) -> None:
         self._client = openai.OpenAI()
 
     def run(self, prompt: str, toolbox: Toolbox) -> BackendRun:
+        # TODO: Switch to the thread run API, using tools to leverage toolbox
+        # methods.
+        # https://platform.openai.com/docs/assistants/deep-dive#runs-and-run-steps
+        # https://github.com/openai/openai-python/blob/main/src/openai/resources/beta/threads/runs/runs.py
         completion = self._client.chat.completions.create(
             messages=[
+                {"role": "system", "content": _SYSTEM_PROMPT},
                 {"role": "user", "content": prompt},
             ],
             model="gpt-4o",
