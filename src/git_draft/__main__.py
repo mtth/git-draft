12954e567ecc9a461a0f9186d6b5a96baa67ce73
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-03-02 10:51:46 -0800

    feat: move logging to state file

--- src/git_draft/__main__.py
+++ src/git_draft/__main__.py
@@ -4,93 +4,88 @@ import importlib.metadata
 import logging
 import optparse
 import sys
-import textwrap
 
 from .bots import load_bot
-from .common import Store, open_editor
+from .common import Config, PROGRAM, Store, ensure_state_home, open_editor
 from .manager import Manager
 
 
-logging.basicConfig(level=logging.INFO)
-
-
-EPILOG = """\
-    More information via `man git-draft` and https://mtth.github.io/git-draft.
-"""
-
-
-parser = optparse.OptionParser(
-    prog="git-draft",
-    epilog=textwrap.dedent(EPILOG),
-    version=importlib.metadata.version("git_draft"),
-)
-
-parser.disable_interspersed_args()
-
-
-def add_command(name: str, **kwargs) -> None:
-    def callback(_option, _opt, _value, parser) -> None:
-        parser.values.command = name
-
-    parser.add_option(
-        f"-{name[0].upper()}",
-        f"--{name}",
-        action="callback",
-        callback=callback,
-        **kwargs,
+def initialize_logging(config: Config) -> None:
+    logging.basicConfig(
+        level=config.log_level,
+        filename=str(ensure_state_home() / "log"),
     )
 
 
-add_command("discard", help="discard the current draft")
-add_command("finalize", help="apply the current draft to the original branch")
-add_command("generate", help="start a new draft from a prompt")
+def new_parser() -> optparse.OptionParser:
+    parser = optparse.OptionParser(
+        prog=PROGRAM,
+        version=importlib.metadata.version("git_draft"),
+    )
 
-parser.add_option(
-    "-a",
-    "--bot",
-    dest="bot",
-    help="bot key",
-    default="openai",
-)
-parser.add_option(
-    "-c",
-    "--checkout",
-    help="check out generated changes",
-    action="store_true",
-)
-parser.add_option(
-    "-d",
-    "--delete",
-    help="delete draft after finalizing or discarding",
-    action="store_true",
-)
-parser.add_option(
-    "-p",
-    "--prompt",
-    dest="prompt",
-    help="draft generation prompt, read from stdin if unset",
-)
-parser.add_option(
-    "-r",
-    "--reset",
-    help="reset index before generating a new draft",
-    action="store_true",
-)
-parser.add_option(
-    "-s",
-    "--sync",
-    help="commit prior worktree changes separately",
-    action="store_true",
-)
+    parser.disable_interspersed_args()
 
+    def add_command(name: str, **kwargs) -> None:
+        def callback(_option, _opt, _value, parser) -> None:
+            parser.values.command = name
 
-EDITOR_PLACEHOLDER = """\
-    Enter your prompt here...
-"""
+        parser.add_option(
+            f"-{name[0].upper()}",
+            f"--{name}",
+            action="callback",
+            callback=callback,
+            **kwargs,
+        )
+
+    add_command("discard", help="discard the current draft")
+    add_command("finalize", help="apply current draft to original branch")
+    add_command("generate", help="start a new draft from a prompt")
+
+    parser.add_option(
+        "-a",
+        "--bot",
+        dest="bot",
+        help="bot key",
+        default="openai",
+    )
+    parser.add_option(
+        "-c",
+        "--checkout",
+        help="check out generated changes",
+        action="store_true",
+    )
+    parser.add_option(
+        "-d",
+        "--delete",
+        help="delete draft after finalizing or discarding",
+        action="store_true",
+    )
+    parser.add_option(
+        "-p",
+        "--prompt",
+        dest="prompt",
+        help="draft generation prompt, read from stdin if unset",
+    )
+    parser.add_option(
+        "-r",
+        "--reset",
+        help="reset index before generating a new draft",
+        action="store_true",
+    )
+    parser.add_option(
+        "-s",
+        "--sync",
+        help="commit prior worktree changes separately",
+        action="store_true",
+    )
+
+    return parser
 
 
 def main() -> None:
-    (opts, _args) = parser.parse_args()
+    config = Config.load()
+    initialize_logging(config)
+    (opts, _args) = new_parser().parse_args()
 
     manager = Manager.create(Store.persistent())
 
@@ -100,7 +95,7 @@ def main() -> None:
         prompt = opts.prompt
         if not prompt:
             if sys.stdin.isatty():
-                prompt = open_editor(textwrap.dedent(EDITOR_PLACEHOLDER))
+                prompt = open_editor("Enter your prompt here...")
             else:
                 prompt = sys.stdin.read()
         manager.generate_draft(
