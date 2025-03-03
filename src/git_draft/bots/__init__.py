4cbe5411744eb5e14688d54667bf7f8dcd2edb81
Matthieu Monsch <matthieu.monsch@gmail.com>
2025-03-02 18:45:15 -0800

    feat: read bot config

--- src/git_draft/bots/__init__.py
+++ src/git_draft/bots/__init__.py
@@ -3,8 +3,10 @@
 * https://aider.chat/docs/leaderboards/
 """
 
-from typing import Any, Mapping
+import importlib
+import sys
 
+from ..common import BotConfig
 from .common import Action, Bot, Operation, OperationHook, Toolbox
 
 __all__ = [
@@ -16,13 +18,27 @@ __all__ = [
 ]
 
 
-def load_bot(entry: str, kwargs: Mapping[str, Any]) -> Bot:
-    if entry == "openai":
-        return _load_openai_bot(**kwargs)
-    raise NotImplementedError()  # TODO
+def load_bot(config: BotConfig) -> Bot:
+    if config.pythonpath and config.pythonpath not in sys.path:
+        sys.path.insert(0, config.pythonpath)
+
+    parts = config.factory.split(":", 1)
+    if len(parts) == 1:
+        module = sys.modules[__name__]  # Default to this module
+        symbol = parts[0]
+    else:
+        module_name, symbol = parts
+        module = importlib.import_module(module_name)
+
+    factory = getattr(module, symbol, None)
+    if not factory:
+        raise NotImplementedError(f"Unknown factory: {factory}")
+
+    kwargs = config.config or {}
+    return factory(**kwargs)
 
 
-def _load_openai_bot(**kwargs) -> Bot:
+def openai_bot(**kwargs) -> Bot:
     from .openai import OpenAIBot
 
     return OpenAIBot(**kwargs)
